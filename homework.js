e7c56f0e7d84aff08bc11366365eae2013cd4313
VladOstapchuk <vlad.ostapchuk@gmail.com>
2025-01-06 16:15:12 +0200

    first

---
+++ homework.js
@@ -0,0 +1,30 @@
+// Написати функцію pow(x,y) яка буде приймати 2 цисла, перше це число яке ми будемо зводити у ступінь, друге у яку ступінь.
+
+// Наприклад pow(2,3) функція поверне значення 8.
+
+function pow(a, b) {
+  let result;
+  if (isNaN(a) || isNaN(b)) {
+    console.log("Please input correct value (must be number)");
+    console.log("Type of some value is " + typeof a);
+  }
+  if (b === 0) {
+    result = 1;
+  }
+  if (b === 1) {
+    result = a;
+  }
+  if (b < 0) {
+  }
+  if (b > 0 || b < 1) {
+  } else {
+    result = a;
+    for (let i = 0; i < b; i++) {
+      result = result * a;
+    }
+  }
+
+  console.log("Result is: " + result);
+}
+
+pow(2, 3);
